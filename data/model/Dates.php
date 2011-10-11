f1b22b656d8187633a6657816c6e677bb8ef7620
Joe Beeson <jbeeson@gmail.com>
2011-10-11 10:44:50 -0500

    Adding the Dates and Uuid filters.

---
+++ data/model/Dates.php
@@ -0,0 +1,25 @@
+<?php
+
+	// Retrieve required libraries.
+
+	/**
+	 * dates.php
+	 *
+	 * Filter for automatically adding `created` and `modified` dates
+	 * for records.
+	 *
+	 * @author Joe Beeson <jbeeson@gmail.com>
+	 */
+	return function($self, $params, $chain) {
+		$schema = $self::schema();
+		$string = date('Y-m-d H:i:s');
+		if (!$params['entity']->exists() and array_key_exists('created', $schema)) {
+			if ($schema['created']['type'] === 'datetime' and empty($params['date']['created'])) {
+				$params['data']['created'] = $string;
+			}
+		}
+		if (array_key_exists('modified', $schema)) {
+			$params['data']['modified'] = $string;
+		}
+		return $chain->next($self, $params, $chain);
+	};
---
+++ data/model/Uuid.php
@@ -0,0 +1,24 @@
+<?php
+
+	// Retrieve required libraries.
+	use lithium\util\String;
+
+	/**
+	 * uuid.php
+	 *
+	 * Filter for automatically creating UUIDs for models that employ
+	 * them for primary keys.
+	 *
+	 * @author Joe Beeson <jbeeson@gmail.com>
+	 */
+	return function($self, $params, $chain) {
+		if (!$params['entity']->exists()) {
+			if (array_intersect_assoc(
+				array('type' => 'string', 'length' => 36),
+				$self::schema($self::meta('key'))
+			)) {
+				$params['data'][$self::meta('key')] = String::uuid();
+			}
+		}
+		return $chain->next($self, $params, $chain);
+	};
